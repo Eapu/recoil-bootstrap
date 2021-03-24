70ffa072c307434194453ced96a1537d868db1d6
lna <elena.andresprado@gmail.com>
2021-03-24 10:26:03 +0000

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { RecoilRoot } from 'recoil';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from '../components/navbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+    <Navigation />
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,61 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
-export default function Home() {
+import {
+  atom,
+  useRecoilState,
+  useResetRecoilState,
+  selector,
+  useRecoilValue,
+} from 'recoil';
+
+const countAtom = atom({
+  key: 'count-atom',
+  default: 10,
+});
+
+const fontSizeSelector = selector({
+  key: 'font-size-selector',
+  get: ({ get }) => {
+    const count = get(countAtom);
+    const fontSize = count * 4;
+    return fontSize;
+  },
+});
+
+const Index = (props) => {
+  const [count, setCount] = useRecoilState(countAtom);
+  const resetCount = useResetRecoilState(countAtom);
+  const fontSize = useRecoilValue(fontSizeSelector);
+
+  const countValue = useRecoilValue(countAtom);
+  const percentage = useRecoilValue(countAtom)
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Container fluid>
+          <ProgressBar now={percentage} label={`${percentage}% completed`} />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <br></br>
+      <h2>The count value is : {count} </h2>
+      <h3>The font size of potty is : {fontSize} px </h3>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <Button onClick={() => setCount(count + 1)}>Increase count</Button>
+      <Button onClick={resetCount}>reset count</Button>
+      
+      <Row className="justify-content-md-center">
+    
+        <p style={{ fontSize: fontSize }}>👾</p>
+ 
+      </Row>
+      </Container>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+  );
 }
+
+
+
+export default Index;
+
